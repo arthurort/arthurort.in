241c99b1e18b40a5aa53f9e5ca8aef982821c086
Arthur Ortin <arthurort@users.noreply.github.com>
2017-08-31 11:21:21 +0200

    Création d'un CSS spécifique à la frontpage

--- header.php
+++ header.php
@@ -18,12 +18,16 @@
         })(window, document, 'script', 'dataLayer', 'GTM-PW2DCDC');
     </script>
     <!-- End Google Tag Manager -->
+
     <meta name="theme-color" content="#f2c400">
     <meta name="apple-mobile-web-app-status-bar-style" content="#f2c400">
     <link rel="shortcut icon" href="assets/favico.png">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <?php if( is_front_page() ) { ?>
+        <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/assets/css/frontpage.css" />
+    <?php } ?>
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
-<body>
+<body<?php body_class(); ?>>
