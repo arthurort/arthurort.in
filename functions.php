31ee608533d49c8727ca6cddbdbae1e1afc65c04
Arthur Ortin <arthurort@users.noreply.github.com>
2017-08-31 14:55:01 +0200

    ajout de bootstrap 4

--- functions.php
+++ functions.php
@@ -6,7 +6,14 @@ add_theme_support('post-thumbnails');
         // wp_enqueue_style("fontMonserrat_css", 'https://fonts.googleapis.com/css?family=Montserrat');
         wp_enqueue_style("master_css", get_template_directory_uri() . '/assets/css/master.css' );
         wp_enqueue_style("google_css", get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css');
+        wp_enqueue_style("bootstrap_css", get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
     }
 
 add_action('wp_enqueue_scripts', 'wpao_theme_styles');
+    function theme_js() {
+
+    	global $wp_scripts;
+    	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/assets/js/bootstrap.min.js' );
+}
+
  ?>
