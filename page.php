b097dbe9af9b766afd63f93e425d8a90a37e1bfb
Lettow <arthur.ortin@gmail.com>
2017-09-10 16:17:25 +0200

    Changement de nom du template

--- page.php
+++ page.php
@@ -1,13 +1,13 @@
 <?php
     /*
-    Template Name: liste
+    Template Name: Page
     */
 ?>
 <?php get_header() ?>
 <section>
     <?php if (have_posts() ) : while( have_posts()) : the_post(); ?>
         <h1><?php the_title() ?></h1>
-        <p><?php the_content() ?></p>
+        <?php the_content() ?>
     <?php endwhile; else : ?>
         <?php _e('Page non-trouvée.') ?>
     <?php endif; ?>
