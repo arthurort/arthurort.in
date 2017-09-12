b0b45df4b5141d4f18f5321d5e5893d786cb4102
Arthur Ortin <arthurort@users.noreply.github.com>
2017-09-12 13:32:17 +0200

    ajout de la page 404.php

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div id="wrapper">
+    <section>
+        <h2>404 - Cette page n'existe pas</h2>
+
+        <p>Il n'y a pas de page ici. Avez-vous essayé de chercher au dernier endroit où vous l'avez vu ?</p>
+    </section>
+</div>
+<?php get_footer(); ?>
