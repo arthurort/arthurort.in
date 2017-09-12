4dcfc74cc0f74a90b048f3dc0af7f854a2aa97bb
Arthur Ortin <arthurort@users.noreply.github.com>
2017-09-12 14:10:56 +0200

    Mise en page de la page 404

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 
-<div id="wrapper">
-    <section>
-        <h2>404 - Cette page n'existe pas</h2>
 
-        <p>Il n'y a pas de page ici. Avez-vous essayé de chercher au dernier endroit où vous l'avez vu ?</p>
+    <section class="card container  m-b-3 text-xs-center ">
+        <h2>404 -  Cette page n'existe pas</h2>
+
+        <p class="lead">Il n'y a pas de page ici.</p>
+        <p>Avez-vous essayé de chercher au dernier endroit où vous l'avez vu ?</p>
     </section>
-</div>
+
 <?php get_footer(); ?>
